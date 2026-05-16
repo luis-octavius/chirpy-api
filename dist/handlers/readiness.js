8a1846958f3f3c4556d2483fdc1b5b15774913e3
luis-octavius <luisoctavius.sc@gmail.com>
2026-05-15 21:56:57 -0300

    chore: add dist in gitignore

--- dist/handlers/readiness.js
+++
@@ -1,6 +0,0 @@
-export async function handlerReadiness(req, res) {
-    res.set({
-        "Content-Type": "text/plain; charset=utf-8",
-    });
-    res.send("OK");
-}
--- dist/index.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { handlerReadiness } from "./handlers/readiness.js";
-const app = express();
-const PORT = 8080;
-app.use("/app", express.static("./src/app"));
-app.get("/healthz", handlerReadiness);
-app.listen(PORT, () => {
-    console.log(`Server listening on port: ${PORT}...`);
-});
