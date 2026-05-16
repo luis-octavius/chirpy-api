6fd038fb7fe51a51744a0439c47b459f197efdf2
luis-octavius <luisoctavius.sc@gmail.com>
2026-05-16 20:26:06 -0300

    refactor(api): change endpoints that do not serve static files by prepending /api to the path

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,20 @@
 import express from "express";
 import { handlerReadiness } from "./handlers/readiness.js";
 import { middlewareLogResponses } from "./middleware/log.js";
+import { middlewareMetricsInc } from "./middleware/metrics.js";
+import { handlerMetrics, handlerResetMetrics } from "./handlers/metrics.js";
 
 const app = express();
 const PORT = 8080;
 
+app.use("/app", middlewareMetricsInc);
 app.use("/app", express.static("./src/app"));
 
 app.use(middlewareLogResponses);
 
-app.get("/healthz", handlerReadiness);
+app.get("/api/healthz", handlerReadiness);
+app.get("/api/metrics", handlerMetrics);
+app.get("/api/reset", handlerResetMetrics);
 
 app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}...`);
