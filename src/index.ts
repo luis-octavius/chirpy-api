3c1a60e05f02b6f09318c0fb8321cfa71e316e32
luis-octavius <luisoctavius.sc@gmail.com>
2026-05-15 21:57:29 -0300

    feat(api): add middleware to log non successful requests

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import { handlerReadiness } from "./handlers/readiness.js";
+import { middlewareLogResponses } from "./middleware/log.js";
 
 const app = express();
 const PORT = 8080;
 
 app.use("/app", express.static("./src/app"));
+
+app.use(middlewareLogResponses);
 
 app.get("/healthz", handlerReadiness);
 
