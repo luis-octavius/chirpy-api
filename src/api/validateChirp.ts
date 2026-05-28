48bc3d290957143085f1826ce83a1c1140ad0640
luis-octavius <luisoctavius.sc@gmail.com>
2026-05-27 21:36:50 -0300

    feat(api): add word filtering at validateChirp endpoint

--- src/api/validateChirp.ts
+++ src/api/validateChirp.ts
@@ -6,14 +6,34 @@ export async function handlerValidateChirp(req: Request, res: Response) {
     body: string;
   };
 
-  const params: parameters = req.body;
+  const MAX_CHIRP_LENGTH = 140;
 
-  const maxChirpLength = 140;
-  if (params.body.length >= maxChirpLength) {
-    respondWithError(res, 400, "Chirp is too long");
+  try {
+    const { body } = req.body as parameters;
+
+    if (body.length >= MAX_CHIRP_LENGTH) {
+      return respondWithError(res, 400, "Chirp is too long");
+    }
+
+    const filteredBody = filterWords(body);
+
+    respondWithJSON(res, 200, {
+      cleanedBody: filteredBody,
+    });
+  } catch (error) {
+    return respondWithError(res, 500, "Internal server error");
+  }
+}
+
+function filterWords(body: string) {
+  const words = body.split(" ");
+
+  for (let i = 0; i < words.length; i++) {
+    const word = words[i].toLowerCase();
+    if (word === "kerfuffle" || word === "sharbert" || word === "fornax") {
+      words[i] = "****";
+    }
   }
 
-  respondWithJSON(res, 200, {
-    valid: true,
-  });
+  return words.join(" ").trim();
 }
