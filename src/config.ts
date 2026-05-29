148963947c9b9ff7705f172525dcdc4836e76669
luis-octavius <luisoctavius.sc@gmail.com>
2026-05-29 20:41:04 -0300

    feat(db): add postgres database service to the application
    - install drizzle and drizzle-kit to generate and migrate database
    - create a schema and a config for drizzle
    - create users database and a query to create a user

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,18 @@
+process.loadEnvFile();
+
 type APIConfig = {
   fileServerHits: number;
+  dbUrl: string;
 };
 
 export const config = {
   fileServerHits: 0,
+  dbUrl: envOrThrow("DB_URL"),
 } as APIConfig;
+
+function envOrThrow(key: string) {
+  const variable = process.env.key;
+  if (!variable) throw new Error(`${key} not set in env file`);
+
+  return variable;
+}
