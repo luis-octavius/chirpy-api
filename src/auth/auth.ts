15bee37b0fe8453ec9bd94f5de9ca3eadd5dc122
luis-octavius <luisoctavius.sc@gmail.com>
2026-06-03 19:52:45 -0300

    feat(auth): add JWT creation and validation

--- src/auth/auth.ts
+++ src/auth/auth.ts
@@ -1,4 +1,7 @@
 import * as argon from "argon2";
+import jwt from "jsonwebtoken";
+import type { payload } from "../types/auth.js";
+import { UnauthorizedError } from "../api/errors.js";
 
 export async function hashPassword(password: string): Promise<string> {
   try {
@@ -21,5 +24,31 @@ export async function checkPasswordHash(
     }
   } catch (err) {
     throw new Error("Internal server error");
+  }
+}
+
+export function makeJWT(
+  userID: string,
+  expiresIn: number,
+  secret: string,
+): string {
+  const iat = Math.floor(Date.now() / 1000); // current time in seconds
+  const payload: payload = {
+    iss: "chirpy",
+    sub: userID,
+    iat: iat,
+    exp: iat + expiresIn,
+  };
+
+  const token = jwt.sign(payload, secret, { algorithm: "HS256" });
+  return token;
+}
+
+export function validateJWT(tokenString: string, secret: string): string {
+  try {
+    const payload = jwt.verify(tokenString, secret) as payload;
+    return payload.sub as string;
+  } catch (err) {
+    throw new UnauthorizedError("Token is invalid");
   }
 }
